100032cbb77bf07cdcbde4c7857d32f45407196f
anonymous anonymous <jake.erichson@gmail.com>
2022-03-10 20:35:50 +0000

    Validate presence of gradings.teacher_id

--- app/models/grading.rb
+++ app/models/grading.rb
@@ -5,6 +5,8 @@ class Grading < ApplicationRecord
 
   # Validations
 
+  validates :teacher_id, :presence => true
+
   # Scopes
 
   def to_s
--- spec/models/grading_spec.rb
+++ spec/models/grading_spec.rb
@@ -12,5 +12,7 @@ RSpec.describe Grading, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:teacher_id) }
+
     end
 end
